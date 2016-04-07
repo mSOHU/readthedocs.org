1780ee1ff83bdbfb1c40187d582d4ab437e52efc
boyxuper <boyxuper@gmail.com>
2016-04-07 13:20:41 +0800

    set default options

--- readthedocs/wsgi.py
+++ readthedocs/wsgi.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.postgres")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "readthedocs.settings.sqlite")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
